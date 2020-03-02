47457f438ec0b1112bd11cc58f006e5e526237be
yoongaemii <yoonhy557@gmail.com>
2020-03-02 22:26:06 +0900

    w7

--- w7_deleteNodes.py
+++ w7_deleteNodes.py
@@ -12,15 +12,15 @@ class TreeNode:
 
 
 class Solution:
-    def delNodes(self, root, to_delete):
-        '''
+    '''
         초기 풀이의 문제점
         1.
         클래스 함수 delNodes 내에서 forest라는 변수를 초기화하고 이를 BFS라는 함수를 호출하며 변경하고 싶은데 UnboundLocalError: local variable 'forest' referenced before assignment 가 뜬다. 클래스변수로 선언하면 self.forest를 통해 원하는대로 작동시킬 수 있으나 [root]로 초기화할 수 없다. -> 클래스 함수 안에 self.forest=[root]
         2. 노드가 업데이트 되지 않는다
         root=None으로 하는 것만으로는 안된다. root라는 변수가 left혹은 right의 주소를 가져오는 게 아니라 값을 가져오기 때문에. 
         방향을 바꿔서 to_delete를 자식노드로 가지고 있는 부모의 입장에서 생각해보자
-        '''
+    '''
+    def delNodes(self, root, to_delete):    
         self.forest = [root]
         def BFS(root):
             if root == None:
@@ -79,3 +79,26 @@ class Solution:
 root = TreeNode(1, TreeNode(2), TreeNode(3,right=TreeNode(4)))
 s = Solution()
 s.delNodes(root, [2,1])
+
+class Solution:
+    '''
+    https://leetcode.com/problems/delete-nodes-and-return-forest/discuss/328853/JavaC%2B%2BPython-Recursion-Solution
+    아이디어: node가 root이고 to_delete에 없다면 result에 더해준다
+    나의 풀이에서 본인이 아니라 부모 측에서 to_delete 여부를 판단했던 것은 삭제할 노드의 경우 부모의 left 혹은 right를 변경해야하기 때문이었다. 본 솔루션에서는 recursive helper에서 삭제될 노드라면 None을 반환하게 하고 root.left = helper(*args), root.right = helper(*args)로 helper를 활용하여 본인 위치에서 판단하고 그 결과를 부모 측에서 활용하도록 설계했다
+    '''
+    def delNodes(self, root, to_delete):
+        to_delete_set = set(to_delete) # 속도 줄이는 간단한 trick
+        res = []
+
+        def helper(root, is_root):
+            if not root: return None
+            deleted = root in to_delete_set # is_root arguement 전달으로 지저분한 if문을 대체하는 trick
+            if not deleted and is_root:
+                res.append(root)
+                
+            root.left = helper(root.left, deleted) # 삭제되면 children이 root, 삭제되지 않으면 is_root=False
+            root.right = helper(root.right, deleted)
+            return None if deleted else root
+
+
+        helper(root, True)
